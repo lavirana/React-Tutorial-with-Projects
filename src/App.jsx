8f7345c21f1d8f90d6d8fc9f9b7981729208b799
Ashish Rana <ashishrana288@gmail.com>
2026-04-29 10:30:47 +0530

    added chat messages

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,36 @@ import ChatMessage from "./components/ChatMessage";
 import ChatInput from "./components/ChatInput";
 
 function App() {
+  const chatMessages = [
+    {
+      message: "hello chatbot",
+      sender: "user",
+    },
+    {
+      message: "hello! how can i help you",
+      sender: "robot",
+    },
+    {
+      message: "what time is it?",
+      sender: "user",
+    },
+    {
+      message: "its 3:00 pm",
+      sender: "robot",
+    },
+  ];
+
   return (
     <div className="chat-app">
       <div className="messages-area">
-        <ChatMessage message="Hello Chatbot" sender="user" />
-        <ChatMessage message="Hello! How can i help you" sender="robot" />
-        <ChatMessage message="What time is it?" sender="user" />
-        <ChatMessage message="its 3:00 pm" sender="robot" />
+        {chatMessages.map((chatMessage) => {
+          return (
+            <ChatMessage
+              message={chatMessage.message}
+              sender={chatMessage.sender}
+            />
+          );
+        })}
       </div>
 
       <div className="input-area">
