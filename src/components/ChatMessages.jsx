eebb1a9afe8a56e06a1690ed4054834357ab8b7d
Ashish Rana <ashishrana288@gmail.com>
2026-04-30 11:56:33 +0530

    chat bot all code

--- src/components/ChatMessages.jsx
+++ src/components/ChatMessages.jsx
@@ -1,6 +1,11 @@
+import React from "react";
 import ChatMessage from "./ChatMessage";
 
 const ChatMessages = ({ chatMessages }) => {
+  React.useEffect(() => {
+    const messagesArea = document.querySelector(".messages-area");
+    messagesArea.scrollTop = messagesArea.scrollHeight;
+  }, [chatMessages]);
   return (
     <>
       {chatMessages.map((chatMessage) => (
